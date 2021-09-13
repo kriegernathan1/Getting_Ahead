c85fb0203596def655886e9b352abf26b8cdbf64
Nathan Krieger <nkrieger0444@gmail.com>
2021-09-12 20:37:32 -0700

    deploy config

--- day_scheduler/day_tasker/day_tasker/settings.py
+++ day_scheduler/day_tasker/day_tasker/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'zw3)jz$0c*73*o8n%a5il4r#wfsow4x2=hvz=ioy#q^(oz3a1)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
